5cc2cb9610731bf034de0e0b89f0f2e5a332d36d
Saurabh Thakur <76144109+saurabh-ankit@users.noreply.github.com>
2024-04-03 19:10:23 +0530

    Update index.php

--- admin/department/index.php
+++ admin/department/index.php
@@ -77,9 +77,9 @@ alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
                                     data-id="<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span>
                                     Edit</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item delete_data" href="javascript:void(0)"
-                                    data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span>
-                                    Delete</a>
+                                <!--  <a class="dropdown-item delete_data" href="javascript:void(0)"
+                                    data-id="<?php //echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span>
+                                    Delete</a> -->
                             </div>
                         </td>
                     </tr>
